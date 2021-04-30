1d9e62c057898a59bf90d4c11e60509a98717e77
12189108 <zhoumeng200012@163.com>
2021-04-30 18:20:16 +0800

    完善应用中，添加获取课程失败的提示

--- app/src/main/java/waste/time/yuketang/ProgressActivity.java
+++ app/src/main/java/waste/time/yuketang/ProgressActivity.java
@@ -1,6 +1,11 @@
 package waste.time.yuketang;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.KeyEvent;
 
 import org.json.JSONException;
@@ -12,13 +17,15 @@ import Support.HttpSupport;
 public class ProgressActivity extends BaseActivity {
     private JSONObject data;
     private String video_url,cookie,uv_id;
-
+    private AlertDialog load_dialog;
+    private String[] msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initActivity(this);
         cookie=getIntent().getStringExtra("cookie");
         uv_id=getIntent().getStringExtra("uv_id");
+        load_dialog=DialogFactorySupport.SimpleProgressDialog(getstring(R.string.course_load),false);
         try { 
             data=new JSONObject(getIntent().getStringExtra("msg"));
             video_url=getstring(R.string.video_id_url).replace("*",data.getString("classroom_id")).replace("#",data.getString("course_sign")).replace("%",uv_id);
@@ -35,17 +42,37 @@ public class ProgressActivity extends BaseActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
-    private String getVideoIds(){
+    private String[] getVideoIds(){
         HttpSupport http=new HttpSupport();
         http.init(video_url);
         http.AttachCookie(cookie).AttachUser_Agent(getstring(R.string.user_agent)).AttachProperty("university-id",uv_id).AttachProperty("xtbz","cloud");
-        return http.getHtml()[1];
+        return http.getHtml();
+    }
+    @SuppressLint("HandlerLeak")
+    private class Handlers extends Handler{
+        public Handlers(){
+            super(getMainLooper());
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 0:
+                    break;
+                case 1:
+                    ShortToastFactorySupport.makeText(getstring(R.string.get_course_failed),R.drawable.bcp).show();
+                    finish();
+                    break;
+            }
+        }
     }
     private class Process extends Thread{
         @Override
         public void run() {
             super.run();
-            SystemServiceSupport.CopytoSystem(getVideoIds());
+            msg=getVideoIds();
+            if(msg[0].equals("200"))new Handlers().sendEmptyMessage(0);
+            else new Handlers().sendEmptyMessage(1);
         }
     }
 }
